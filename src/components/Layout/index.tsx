43ffa2a5cf76a52060910245bb7322b6a848f30d
vinit717 <vinit224488@gmail.com>
2024-08-13 16:44:20 +0530

    chore: add rtk

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { FC, ReactNode } from 'react';
+
+// import Navbar from '@/components/Navbar';
+
+type LayoutProps = {
+    title: string;
+    children: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ title, children }) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+                <meta
+                    name="description"
+                    content="Ecommerce assignment"
+                />
+                <meta name="author" content="Vinit" />
+                <meta property="og:title" content="Profilyi - E-commerce" />
+                <meta
+                    property="og:description"
+                    content="Ecommerce assignment of Profyli"
+                />
+                <meta property="og:type" content="website" />
+            </Head>
+            {/* <Navbar /> */}
+            <main>{children}</main>
+        </>
+    );
+};
+
+export default Layout;
---
+++ src/pages/app/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+
+
+const App = () => {
+    return (
+        <Layout title="Home | Profyli">
+            <div>E commerce</div>
+        </Layout>
+    );
+};
+
+export default App;
